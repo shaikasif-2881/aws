28c9e731ec3f92aca056c97404e8db10ddd7aad8
shaikasif-2881 <abdulasif881@gmail.com>
2026-01-21 02:29:27 +0530

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -------------------------
+# VPC
+# -------------------------
+resource "aws_vpc" "example1" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+# -------------------------
+# Subnet
+# -------------------------
+resource "aws_subnet" "example_subnet" {
+  vpc_id            = aws_vpc.example1.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+resource "aws_instance" "example" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.example_subnet.id
+
+  tags = {
+    Name = "dev-server"
+  }
+}
+
+
+
+
+
+
+
+
+
+
